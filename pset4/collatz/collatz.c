944af7822b90cb4680d53c75f5b99daa2e2226fb
julra197 <ralfebert@outlook.com>
2017-11-12 13:27:47 +0100

    collatz using recursion added

---
+++ pset4/collatz/collatz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int collatz(int n);
+
+int main(void)
+{
+
+    printf("%d\n", collatz(15));
+}
+
+int collatz(int n)
+{
+    if (n == 1)
+        return 0;
+    else if (n%2 == 0)
+        return 1 + collatz(n/2);
+    else
+        return 1 + collatz(3*n+1);
+}
